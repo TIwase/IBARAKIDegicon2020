fc8bf8a07af01294e9d29a83b978f7eaab4e77ea
TIwase <tanu.z0uz0u@gmail.com>
2020-09-12 01:00:56 +0900

    this is my first commit

---
+++ infraredLight.py
@@ -0,0 +1,33 @@
+import RPi.GPIO as GPIO
+from time import sleep
+
+red = 17
+green = 27
+blue = 22
+infr = 18
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(infr, GPIO.IN)
+
+ports = [red, green, blue]
+for port in ports:
+	GPIO.setup(port, GPIO.OUT)
+
+try:
+        while True:
+		if(GPIO.input(infr) == GPIO.HIGH):
+			GPIO.output(red,1)
+			GPIO.output(green,0)
+			GPIO.output(blue,1)
+			print GPIO.input(infr)
+			sleep(1)
+		elif(GPIO.input(infr) == GPIO.LOW):
+			GPIO.output(red,0)
+			GPIO.output(green,0)
+			GPIO.output(blue,0)
+			print GPIO.input(infr)
+			sleep(1)
+
+except KeyboardInterrupt:
+        pass
+
+GPIO.cleanup()
