9dc0fb46e0f081f7866a43ade8fcf34c25708e3e
SIchiyama <brolilove@yahoo.co.jp>
2020-09-20 17:46:05 +0900

    更新しました

--- mics5524.py
+++
@@ -1,7 +0,0 @@
-import serial
-ser = serial.Serial('/dev/ttyACM0', 115200)
-#ser = serial.Serial('/dev/serial0', 115200)
-while True:
-    String_data = ser.readline()
-    print(String_data)
-ser.close()
