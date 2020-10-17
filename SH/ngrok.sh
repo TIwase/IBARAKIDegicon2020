a9aea09e6f339fd5ec7902f2279491931700cfc3
SIchiyama <brolilove@yahoo.co.jp>
2020-10-17 15:13:20 +0900

    シェルスクリプトの追加

---
+++ SH/ngrok.sh
@@ -0,0 +1,2 @@
+ngrok http --region us --log /var/log/ngrok.log --log-format json --log-level info 8000
+
---
+++ SH/py.sh
@@ -0,0 +1,2 @@
+#!/bin/sh -l
+/home/pi/kaso/bin/python /home/pi/Documents/IBARAKIDegicon2020/IW/app.py|sudo tee /var/log/pp.log
