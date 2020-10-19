b3f5a5c741d9cda8f6d06e14b0dce872bad6104d
TIwase <tanu.z0uz0u@gmail.com>
2020-10-20 03:03:17 +0900

    main.py 赤外線検知&ガスセンサ値ログファイル出力 修正

--- IW/pythonCode/main.py
+++ IW/pythonCode/main.py
@@ -7,10 +7,11 @@ import RPi.GPIO as GPIO
 from time import sleep
 from logging import getLogger, StreamHandler, FileHandler, DEBUG
 
-red = 17
-green = 27
-blue = 22
-infr = 18
+red = 22
+green = 17
+blue = 27
+infr = 13
+ports = [red, green, blue]
 buzzer = 25
 flag = 0
 gasTH = 300
@@ -19,13 +20,13 @@ ser = serial.Serial('/dev/ttyACM0', 115200)
 url = "https://notify-api.line.me/api/notify"
 token = os.getenv('LINE_NOTIFY_TOKEN', None)
 headers = {"Authorization" : "Bearer "+ token}
+logpath = os.getcwd()
 
 # ----------------------------------------
 # 初期設定
 def init():
     GPIO.setmode(GPIO.BCM)
     GPIO.setup(infr, GPIO.IN)
-    ports = [red, green, blue, buzzer]
     for port in ports:
         GPIO.setup(port, GPIO.OUT)
         GPIO.output(port,0)
@@ -39,25 +40,33 @@ def getInfrared():
         GPIO.output(blue, 0)
         sleep(1)
 
+    elif(GPIO.input(infr) == GPIO.LOW):
+        for port in ports:
+            GPIO.output(port,0)
+        sleep(1)
+
 # ----------------------------------------
 # ガスセンサ値取得
 def getGasVal():
     rawVal = ser.readline()
-    convertedVal = rawVal.strip().decode('utf-8')
-    return convertedVal
+    convVal = int(rawVal.strip().decode('utf-8'))
+    return convVal
 
 # ----------------------------------------
 # 5分毎にガスセンサ値のログを出力
 def outGasLog(dt_now, gasVal):
-    if int(dt_now.strftime('%M')) % 5 == 0:
-        logfile = '\\var\\log\\mics5524\\' + dt_now.strftime('%Y-%m-%d') + '_gas.log'
+    if int(dt_now.strftime('%M')) % 5 == 0 and int(dt_now.strftime('%S'))  == 0:
+        logfile = logpath + '/' + dt_now.strftime('%Y-%m-%d') + '_gas.log'
         logger = getLogger(dt_now.strftime('%Y-%m-%d %H:%M %S'))    # 同じ名前を付けるとログ出力が重複されるので注意
         sh = StreamHandler()
         logger.setLevel(DEBUG)
         logger.addHandler(sh)
         fh = FileHandler(logfile)
         logger.addHandler(fh)
-        logger.log(20, dt_now.strftime('%Y-%m-%d %H:%M %S') + ': [INFO] ' + gasVal)
+        if gasVal < gasTH:
+            logger.log(20, dt_now.strftime('%Y-%m-%d %H:%M %S') + ': [INFO] ' + str(gasVal))
+        elif gasVal > gasTH:
+            logger.log(30, dt_now.strftime('%Y-%m-%d %H:%M %S') + ': [WARN] The current gas value exceeded the threshold' + str(gasVal))
 
 if __name__ == '__main__':
     print("START GAS DETECTION!")
@@ -67,9 +76,9 @@ if __name__ == '__main__':
             dt_now = datetime.datetime.now()
             gasVal = getGasVal()
             outGasLog(dt_now, gasVal)
-            
-            if int(gasVal) > gasTH and GPIO.input(infr) == GPIO.HIGH and flag == 0:
-                getInfrared()
+            getInfrared()
+
+            if gasVal > gasTH and GPIO.input(infr) == GPIO.HIGH and flag == 0:
                 message = "ガスセンサ値の閾値超過を検知しました。ガス漏れしている可能性があります。"
                 payload = {"message" :  message}
                 r = requests.post(url, headers = headers, params=payload)
